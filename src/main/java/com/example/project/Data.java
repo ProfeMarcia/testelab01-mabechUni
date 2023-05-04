71efc933814ac99af5b2a61b56cd3ccc37a16c19
mabechUni <132605992+mabechUni@users.noreply.github.com>
2023-05-04 17:59:37 +0000

    Envio da primeira parte do trabalho

--- src/main/java/com/example/project/Data.java
+++ src/main/java/com/example/project/Data.java
@@ -1,4 +1,75 @@
 package com.example.project;
 public class Data {
-  
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Data(int dia, int mes, int ano) {
+        //if(! isDataValida(dia, mes, ano))
+        // throw new IllegalArgumentException("A data está errada.");
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public boolean verificaAnoBissexto(int ano) {
+        if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public boolean isDataValida(int dia, int mes, int ano) {
+        if (ano < 1 || mes < 1 || mes > 12 || dia < 1) {
+            return false;
+        }
+        switch (mes) {
+            case 2:
+                if (verificaAnoBissexto(ano)) {
+                    return dia <= 29;
+                } else {
+                    return dia <= 28;
+                }
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return dia <= 30;
+            default:
+                return dia <= 31;
+        }
+    }
+
+    /**
+     * Método que retorna a representação em String da data.
+     */
+    public String toString() {
+        return dia + "/" + mes + "/" + ano;
+    }
 }
--- src/main/java/com/example/project/Loja.java
+++ src/main/java/com/example/project/Loja.java
@@ -7,6 +7,91 @@ package com.example.project;
  */
 public class Loja
 {
-  
+    // instance variables - replace the example below with your own
+    private String nome;
+    private int quantidadeFuncionarios;
+    private double salarioBaseFuncionario;
+    private Endereco endereco;
+    private Data data;
+
+   
+
+
+    /**
+     * Constructor for objects of class Loja
+     */
+    public Loja(String aNome, int aQntFuncionarios, double aSalario)
+    {
+        nome = aNome;
+        quantidadeFuncionarios = aQntFuncionarios;
+        salarioBaseFuncionario = aSalario;
+    }
+    
+    
+    public Loja(String aNome, int aQntFuncionarios)
+    {
+        nome = aNome;
+        quantidadeFuncionarios = aQntFuncionarios;
+        salarioBaseFuncionario = -1;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String aNome) {
+        this.nome = aNome;
+    }
+
+    public int getQuantidadeFuncionarios() {
+        return this.quantidadeFuncionarios;
+    }
+
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios) {
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+
+    public double getSalarioBaseFuncionario() {
+        return this.salarioBaseFuncionario;
+    }
+
+    public void setSalarioFuncioario(double salarioFuncioario) {
+        this.salarioBaseFuncionario = salarioFuncioario;
+    }
+    
+    public String toString(){
+        return "Nome:  " + "Quantidade : " + this.quantidadeFuncionarios;
+    }
+    
+    public double gastosComSalario(){
+        if(this.salarioBaseFuncionario != -1)
+            return  this.quantidadeFuncionarios * salarioBaseFuncionario;
+        return -1;
+    }
+    
+    public Endereco getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Data getData() {
+        return this.data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+    
+    public char tamanhoDaLoja(){
+     if(this.quantidadeFuncionarios >= 10 && this.quantidadeFuncionarios <= 30)
+            return  'M';
+     else if(this.quantidadeFuncionarios >= 31) 
+            return  'G';
+     return 'P';
+    }
+    
     
 }
